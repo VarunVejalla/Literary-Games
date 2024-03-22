ea45ba192b7e62ba40f6e9689ce655f3fa0309fa
Varun Vejalla <varun.vejalla@gmail.com>
2024-03-22 02:24:56 -0400

    Commented player code

--- HW9-SmoothMoves/Scripts/MoveCharacter.gd
+++ HW9-SmoothMoves/Scripts/MoveCharacter.gd
@@ -8,12 +8,15 @@ var gravity : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# sets the gravity vector (straight down)
 	gravity = Vector2(0, 100)
 	pass # Replace with function body.
 
 
 func _get_input():
 	if is_on_floor():
+		# if you're on the floor, and the input is to go left or right, you add accordingly to the velocity accordingly
+		# if the input's to jump, you add a vector pointing up to the velocity
 		if Input.is_action_pressed("move_left"):
 			velocity += Vector2(-movement_speed,0)
 
@@ -24,6 +27,8 @@ func _get_input():
 			velocity += Vector2(1,-jump_height)
 
 	if not is_on_floor():
+		# if you're not on the floor (e.g. you're in the air after having jumped), same as on the floor, but apply the air coefficient
+		# restricts horizontal movement while in the air (physical interpretation - nothing to push off of)
 		if Input.is_action_pressed("move_left"):
 			velocity += Vector2(-movement_speed * horizontal_air_coefficient,0)
 
@@ -31,6 +36,8 @@ func _get_input():
 			velocity += Vector2(movement_speed * horizontal_air_coefficient,0)
 
 func _limit_speed():
+	# If moving horizontally too fast (i.e. moving faster than speed limit), cap speed limit
+	# seems like it'd be more readable to say velocity.x = speed_limit or velocity.x = -speed_limit
 	if velocity.x > speed_limit:
 		velocity = Vector2(speed_limit, velocity.y)
 
@@ -38,17 +45,20 @@ func _limit_speed():
 		velocity = Vector2(-speed_limit, velocity.y)
 
 func _apply_friction():
+	# looks at your friction coefficient and applies friction to your corresponding movement if you're on the ground
 	if is_on_floor() and not (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")):
 		velocity -= Vector2(velocity.x * friction, 0)
 		if abs(velocity.x) < 5:
 			velocity = Vector2(0, velocity.y) # if the velocity in x gets close enough to zero, we set it to zero
 
 func _apply_gravity():
+	# just adds a gravity vector, pointing straight down
 	if not is_on_floor():
 		velocity += gravity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	# does all the phsyics processing steps
 	_get_input()
 	_limit_speed()
 	_apply_friction()
